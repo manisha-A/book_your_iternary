cebf2683e52a855dc32b82bc231ff15ec968a6bc
Manisha Awasthi <itsmanisha_15@yahoo.co.in>
2014-10-01 00:30:07 +0100

    Adding Gemfile and writing pending step definitions

---
+++ features/step_definitions/search_flight_steps.rb
@@ -0,0 +1,8 @@
+Given(/^I am on home page$/) do
+end
+
+When(/^I search one way flight between "(.*?)" and "(.*?)"$/) do |arg1, arg2|
+end
+
+Then(/^I should be able to view available flight options$/) do
+end
