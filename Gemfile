115567b34a21cb069d20f53ec165ff59fb98642b
Manisha Awasthi <itsmanisha_15@yahoo.co.in>
2014-10-01 03:06:26 +0100

    End to end search feature

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'http://rubygems.org'
 
 gem 'cucumber', '~> 1.3.17'
+gem 'selenium-webdriver', '~> 2.43.0'
--- features/step_definitions/search_flight_steps.rb
+++ features/step_definitions/search_flight_steps.rb
@@ -1,8 +1,30 @@
+require 'selenium-webdriver'
+
+
 Given(/^I am on home page$/) do
+  @browser = Selenium::WebDriver.for(:firefox)
+  @browser.navigate.to "http://travelfusion.com"
 end
 
 When(/^I search one way flight between "(.*?)" and "(.*?)"$/) do |arg1, arg2|
+  source = @browser.find_element(:id,'form_city_from')
+  source.send_keys("Atlanta, United States (city)")
+  source.send_keys :enter
+
+  destination = @browser.find_element(:id,'form_city')
+  destination.send_key("London Heathrow Airport [LHR], United Kingdom (airport)")
+  destination.send_keys :enter
+
+  no_of_passengers = @browser.find_element(:id,'form_adults')
+  no_of_passengers.send_keys(2)
+
+  @browser.find_element(:xpath,'/html/body/div[2]/div[2]/table/tbody/tr/td/div[1]/form/div[4]/div/button').click
+  sleep 2
 end
 
 Then(/^I should be able to view available flight options$/) do
+  wait = Selenium::WebDriver::Wait.new(:timeout => 10) # seconds
+  results_header = wait.until{ @browser.find_element(:id,'flight_results')}
+  assert results_header.text.include?("Total Results:"),"Unable to find result header"
+  @browser.quit
 end
