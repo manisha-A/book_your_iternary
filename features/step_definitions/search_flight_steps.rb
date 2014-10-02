d142dc35326f0d604796429a312cb8943deed52f
Manisha <itsmanisha_15@yahoo.co.in>
2014-10-02 02:24:24 +0100

    Green cuke using Capybara

--- features/step_definitions/search_flight_steps.rb
+++ features/step_definitions/search_flight_steps.rb
@@ -4,25 +4,15 @@ Given(/^I am on home page$/) do
   visit Capybara.app_host
 end
 
-When(/^I search one way flight between "(.*?)" and "(.*?)"$/) do |arg1, arg2|
-  source = @browser.find_element(:id,'form_city_from')
-  source.send_keys("Atlanta, United States (city)")
-  source.send_keys :enter
-
-  destination = @browser.find_element(:id,'form_city')
-  destination.send_key("London Heathrow Airport [LHR], United Kingdom (airport)")
-  destination.send_keys :enter
-
-  no_of_passengers = @browser.find_element(:id,'form_adults')
-  no_of_passengers.send_keys(2)
-
-  @browser.find_element(:xpath,'/html/body/div[2]/div[2]/table/tbody/tr/td/div[1]/form/div[4]/div/button').click
-  sleep 2
+When(/^I search one way flight between "(.*?)" and "(.*?)"$/) do |source, destination|
+  fill_in('form_city_from',:with => source)
+  fill_in('form_city',:with => destination)
+  select(2 , :from =>'form_adults')
+  click_button('Search')
+  sleep 15
 end
 
 Then(/^I should be able to view available flight options$/) do
-  wait = Selenium::WebDriver::Wait.new(:timeout => 10) # seconds
-  results_header = wait.until{ @browser.find_element(:id,'flight_results')}
-  assert results_header.text.include?("Total Results:"),"Unable to find result header"
-  @browser.quit
+  assert_text('Total Results:')
+  #find('#flight_results').text.has_content?("Total Results:")
 end
