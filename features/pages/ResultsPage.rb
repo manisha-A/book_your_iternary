a8c50987d56a2a60122968048d30bde29b1dfb69
Manisha <itsmanisha_15@yahoo.co.in>
2014-10-03 19:24:38 +0100

    Refactoring

--- features/pages/ResultsPage.rb
+++ features/pages/ResultsPage.rb
@@ -4,15 +4,7 @@ class ResultPage < Page
   end
 
   def should_be_on_result_tab
-    #result_content = "#flight_results"
     verify_active_tab("Results")
-    #wait_for_element_to_be_present(result_content)
-    @session.find("#flight_results .flight_box", match: :first)
-
-    if (@session.find('#content_field').text == "No Results Found")
-      expect((@session.find('#flight_results').text.include? "Total Results:")).to eq(false)
-    else
-      expect(@session.find('#flight_results').text.include? "Total Results:").to eq(true)
-    end
+    expect(@session.find("#flight_results")).to be
   end
 end
--- features/support/page_support.rb
+++ features/support/page_support.rb
@@ -8,14 +8,6 @@ class Page
   def verify_active_tab(tab_name)
     expect(@session.find('#flight_breadcrumbs .active').text).to eq(tab_name)
   end
-
-  def wait_for_element_to_be_present(selector)
-    #if(@session.has_css?("#{selector}"))
-    @session.should have_content(@session.find("#{selector}").text)
-      #@session.find("#{selector}",match: :first)
-      #expect(page).to have_field("Username", with: "Joe")
-    #end
-  end
 end
 
 module PageHelpers
