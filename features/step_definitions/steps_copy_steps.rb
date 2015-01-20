04c17a4acb83e88c48a4880eff2e89aa67a189d6
Manisha Awasthi <manisha awasthi@appsbroker.com>
2015-01-20 09:59:17 +0000

    Testing tags

---
+++ features/step_definitions/steps_copy_steps.rb
@@ -0,0 +1,19 @@
+Given(/^I am on home page$/) do
+  homepage.navigate_to_home_page
+end
+
+When(/^I search one way flight between "(.*?)" and "(.*?)" for today for (\d+) adults$/) do |source, destination, no_of_adults|
+  homepage.select_source(source)
+  homepage.select_destination(destination)
+  homepage.select_departure_date
+  homepage.select_no_of_adults(no_of_adults)
+  homepage.search_flights
+end
+
+Then(/^I should be on results page$/) do
+  resultpage.should_be_on_result_tab
+end
+
+Then(/^I should see appropriate message to select atleast one adult$/) do
+  homepage.verify_minimum_adults
+end
